dc111f3016f50db23a2983cb98181cd367b08f12
Herbert Ssevume <you@example.com>
2020-03-12 19:15:36 +0000

    Removed trim on search

--- src/Search.js
+++ src/Search.js
@@ -13,7 +13,7 @@ class Search extends Component {
     message = (e)=>{this.setState({message:e.target.value})}
 
     search = (e)=>{e.preventDefault();
-                 this.setState({name:this.state.name.trim()})
+                 this.setState({name:this.state.name})
                 this.props.search(this.state);
                 this.setState({
                                
@@ -37,7 +37,7 @@ class Search extends Component {
                
               <form onSubmit={this.search}>
               <h4>Welcome<span className='loginName'>{this.props.table.loginName}</span>:Type your search below</h4>
-              <input type='text' className='searchInput' name='surname' value={this.state.surname.trim()} placeholder='Type surname' onChange={this.change}/><br/>
+              <input type='text' className='searchInput' name='surname' value={this.state.surname} placeholder='Type surname' onChange={this.change}/><br/>
               <button>Search</button>
               </form>
               <form onSubmit={this.changeMessage}>
