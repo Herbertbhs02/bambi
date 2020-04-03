3c0fae3414f1e32ea6faf59cbd300d690e6f87a8
Herbert Ssevume <you@example.com>
2020-04-03 16:43:08 +0100

    Modified search feature_V6.30

--- src/Search.js
+++ src/Search.js
@@ -5,33 +5,29 @@ class Search extends Component {
     constructor(props) {
       super(props)
       this.state = {name:'',surname:'', message:''}
-      
-    }
+                       }
     
-    change = (e)=>{this.setState({surname:e.target.value})}
+    
+    
+     message = (e)=>{this.setState({message:e.target.value})}
 
-    message = (e)=>{this.setState({message:e.target.value})}
-
-    search = (e)=>{e.preventDefault();
-                 this.setState({name:this.state.name.trim()})
-                this.props.search(this.state);
-                this.setState({
-                               
-                              })}  
+    //search regitered people
+     search = (e)=>{
+                    this.props.search({surname:e.target.value});
+                    this.setState({surname:e.target.value})
+                 }  
 
        /* listall = ()=>{//<button onClick={this.listall}>List all registered</button> //temporary removed
           this.props.listall()}*/
-                                    
+
+            //View messages sent to you by the registered user                        
           view = ()=>{ this.props.view({receiverId:this.props.receiverId})
-            }
-                   
+                     }
+
+      //Updating your personal message/Status             
     changeMessage = (e)=>{e.preventDefault();
-                    this.props.messageUpdate(this.state);
-                    this.setState({
-                                   
-                                  })   
-                                                              
-                       }               
+                    this.props.messageUpdate(this.state);                                      
+                          }               
 
             
     render() {
@@ -39,16 +35,14 @@ class Search extends Component {
         return (
             <div>
                {this.props.table.post}
-              <form onSubmit={this.search}>
-              <h4>Welcome<span className='loginName'> {this.props.table.loginName}</span>. Enter a name and click on search</h4>
-              <input type='text' className='searchInput' name='surname' value={this.state.surname} placeholder='Type surname' onChange={this.change}/><br/>
-              <button>Search</button>
-              </form>
-              
+
+              <h4>Welcome<span className='loginName'> {this.props.table.loginName}</span>. Enter surname. Search is case sensitive!</h4>
+              <input type='text' className='searchInput' name='surname' value={this.state.surname} placeholder='Type surname' onChange={this.search}/><br/>
+
               <form onSubmit={this.changeMessage}>
-              <h4>Update your message</h4>
-              <textarea rows="2" cols="50" name='message' value={this.state.message} placeholder="Leave/update your personal message/status" onChange={this.message}></textarea>
-              <button>Update</button>
+                  <h4>Update your message</h4>
+                  <textarea rows="2" cols="50" name='message' value={this.state.message} placeholder="Leave/update your personal message/status" onChange={this.message}></textarea>
+                  <button>Update</button>
               </form>
 
               <hr/>
